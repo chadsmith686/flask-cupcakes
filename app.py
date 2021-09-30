b2276f64e24b55d8c5bee55b2cf942f92313529f
Chad Smith <chadsmithmusic@icloud.com>
2021-09-29 19:36:42 -0700

    added models.py, and routes to show all/one cupcake

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-"""Flask app for Cupcakes"""
+import re
+from flask import Flask, request, jsonify, render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "secret"
+
+connect_db(app)
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """Returns JSON with all cupcakes"""
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    """Returns JSON about one particular cupcake"""
+    cc = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cc.serialize())
